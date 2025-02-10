5de16c9c9c482f8b5378db4fb027976119969c80
Wesley Soares <wesley.ssoares1@gmail.com>
2025-02-10 02:06:08 -0300

    add tests contract pet

---
+++ src/main/java/com/petstore/utils/TestTags.java
@@ -0,0 +1,8 @@
+package com.petstore.utils;
+
+public class TestTags {
+
+    public static final String FUNCTIONAL = "functional";
+    public static final String CONTRACT = "contract";
+    public static final String PET = "pet";
+}
